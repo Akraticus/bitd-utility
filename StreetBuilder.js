66d40de6cff61f0a777505bd0623a4c0652a23ac
Akraticus <oebl89@gmail.com>
2019-06-21 22:25:16 +0200

    Return statement.

--- StreetBuilder.js
+++ StreetBuilder.js
@@ -2,5 +2,5 @@ const Street = require("./Entities.js").Street;
 const Loader = require("./WeightedCollectionLoader.js");
 
 exports.getStreet = function getStreet(streetOptions){
-
+    return new Street();
 }
